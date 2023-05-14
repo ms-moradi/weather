c74a0bc709ec7a9af5eb6a4bca51cbfb17d03dbb
ms.moradi <ms.moradi28@gmail.com>
2023-05-14 17:10:18 +0430

    add element and styles

--- src/components/favoriteCity/favoriteCity.jsx
+++ src/components/favoriteCity/favoriteCity.jsx
@@ -1,9 +1,18 @@
-
+import Grid from "@mui/material/Grid";
+import Typography  from "@mui/material/Typography";
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
 
 const FavoriteCity =()=>{
     return(
-        <>
-        </>
+        <Grid container sx={{display:'flex',justifyContent:'space-between', borderRadius:'15px',border:'1px solid #fff', padding:'10px 20px', margin:"5px 0 "}}>
+            <Grid>
+                <Typography>تهران</Typography>
+            </Grid>
+            <Grid sx={{display:'flex',justifyContent:'space-evenly',gap:'15px' }}>
+                <WbSunnyIcon sx={{color:'#fff'}}/>
+                <Typography>3</Typography>
+            </Grid>
+        </Grid>
     )
 }
 
