8008d2e890a5c1c71282e875dcb6bd9d37bc1f81
ms.moradi <ms.moradi28@gmail.com>
2023-05-14 17:04:26 +0430

    create and imports element

---
+++ src/components/cardCity/cardCity.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import CustomButton from "../button/button";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import TireRepairIcon from '@mui/icons-material/TireRepair';
+import AirIcon from '@mui/icons-material/Air';
+import OpacityIcon from '@mui/icons-material/Opacity';
+
+const CardCity =()=>{
+    return(
+        <>
+        </>
+    )
+}
+
+export default CardCity;
