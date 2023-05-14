fbe907cc595e7c5031a62d6b9d3ec55d3c291ac1
ms.moradi <ms.moradi28@gmail.com>
2023-05-14 17:17:32 +0430

    add elements and styles

--- src/components/futureDay/futureDay.jsx
+++ src/components/futureDay/futureDay.jsx
@@ -4,8 +4,22 @@ import Typography  from "@mui/material/Typography";
 
 const FutureDay =()=>{
     return(
-        <>
-        </>
+        <Grid container sx={{
+            margin:'0 15px', 
+            display:'flex',
+            flexDirection:{sm:'column'}, 
+            alignItems:'center', 
+            gap:'20px'}}>
+            <Grid sx={{textAlign:'center'}}>
+                <Typography sx={{color:'#fff', width:{xs:'70px', sm:'none'}, fontSize:{xs:'12px', sm:'none'}}}>{cityWeatherFuture?.[index]?.dt_txt}</Typography>
+            </Grid>
+            <Grid>
+                <WbSunnyIcon sx={{fontSize:{xs:'45px',sm:'80px'} , color:'#fff'}}/>
+            </Grid>
+            <Grid>
+                <Typography sx={{color:'#FFF'}}>{cityWeatherFuture?.[index]?.main?.temp}</Typography>
+            </Grid>
+        </Grid>
     )
 }
 
