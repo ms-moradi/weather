4a1cdd044f144f1595e966d9acde7f5f68fa5235
ms.moradi <ms.moradi28@gmail.com>
2023-05-14 16:32:38 +0430

    add  data cities

---
+++ src/data/dataCities.js
@@ -0,0 +1,12 @@
+export const cities = [
+    {id:112931, name:'تهران'},
+    {id:124665, name:'مشهد'},
+    {id:116996, name:'ساری'},
+    {id:118743, name:'رشت'},
+    {id:113646, name:'تبریز'},
+    {id:141681, name:'بندرعباس'},
+    {id:119208, name:'قم'},
+    {id:119505, name:'قزوین'},
+]
+
+export default cities;
