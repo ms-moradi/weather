fa54f8b8e0e819b3a2687fe00b2cb5d1078ab811
ms.moradi <ms.moradi28@gmail.com>
2023-05-14 17:16:52 +0430

    add imports

--- src/components/futureDay/futureDay.jsx
+++ src/components/futureDay/futureDay.jsx
@@ -1,4 +1,6 @@
-
+import Grid from "@mui/material/Grid";
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import Typography  from "@mui/material/Typography";
 
 const FutureDay =()=>{
     return(
