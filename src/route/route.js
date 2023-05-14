064a062d4f41f6a7750ba9ca9daaea201a4626f2
ms.moradi <ms.moradi28@gmail.com>
2023-05-14 16:48:43 +0430

    add import components

--- src/route/route.js
+++ src/route/route.js
@@ -1,18 +1,25 @@
 import { createBrowserRouter } from "react-router-dom";
-
+import Home from "../pages/home/home";
+import Menu from "../pages/menu/menu";
+import SinglePageCity from "../pages/singlePageCity/singlePageCity";
+import Layout from "../components/layout/layout";
 
 const router = createBrowserRouter([
     {
         path:"/",
-        element:"",
+        element: <Layout/>,
         children:[
             {
+                index: true,
+                element: <Home/>
+            },
+            {
                 path:"/menu",
-                element:"",
+                element: <Menu/>,
             },
             {
                 path:"/singlePageCity/:cityID",
-                element:"",
+                element: <SinglePageCity/>,
             },
         ]
     },
