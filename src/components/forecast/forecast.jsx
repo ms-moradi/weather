e3b86d7d53d4faa139a4997d90a106b578fd15b2
ms.moradi <ms.moradi28@gmail.com>
2023-05-14 17:13:21 +0430

    add element and styles

--- src/components/forecast/forecast.jsx
+++ src/components/forecast/forecast.jsx
@@ -1,8 +1,20 @@
 
 const Forecast =()=>{
     return(
-        <>
-        </>
+        <Grid sx={{
+            padding:'30px 0',
+            display:'flex', 
+            flexDirection:'column',
+            }}>
+            <Grid>
+                <Typography sx={{color:'#fff', textAlign:"center", fontSize:{xs:'20px', sm:'40px'}}}>{nameCity}</Typography>
+            </Grid>
+            <Grid sx={{display:'flex',flexDirection:{xs:'column', sm:'row'}, justifyContent:'space-between', gap:'20px', padding:'30px 0'}}>
+                {daysIndex.map(index => 
+                <FutureDay key={index} index={index} cityWeatherFuture={cityWeatherFuture}/>
+                )}
+            </Grid>
+        </Grid> 
     )
 }
 
