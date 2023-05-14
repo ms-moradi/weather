c69be7924ca9adf4572d9a980f938784741e102b
ms.moradi <ms.moradi28@gmail.com>
2023-05-14 17:28:09 +0430

    add imports

--- src/components/dialogSearch/dialogSearch.jsx
+++ src/components/dialogSearch/dialogSearch.jsx
@@ -1,3 +1,12 @@
+import cities from "../../data/dataCities";
+import Dialog from "@mui/material/Dialog";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
 
 
 
