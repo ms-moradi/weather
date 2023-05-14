b8479d6782a5b57133085bef2c59a59b5624dc62
ms.moradi <ms.moradi28@gmail.com>
2023-05-14 17:00:46 +0430

    create button and style

---
+++ src/components/button/button.jsx
@@ -0,0 +1,13 @@
+import Button from "@mui/material/Button";
+
+const Button =({children, borderRadius})=>{
+    return(
+        <>
+            <Button sx={{display:'flex',selfAlign:'flex-start', width:{xs:'200px',sm:'300px'}, border:'1px solid #fff', borderRadius:{borderRadius}, color:'#fff', marginTop:"20px"}}>
+                {children}
+            </Button>
+        </>
+    )
+}
+
+export default Button;
