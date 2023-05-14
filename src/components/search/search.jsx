d58f893b5e6f678a459c8def3f52b3507f1f7a07
ms.moradi <ms.moradi28@gmail.com>
2023-05-14 17:23:49 +0430

    add tag and style

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -7,9 +7,10 @@ import InputBase from '@mui/material/InputBase';
 
 const Search =({setSearch, search, selectCity, setSelectCity})=>{
     return(
-        <>
-            
-        </>
+        <Grid>
+            <InputBase sx={{borderBottom:'2px solid #fff',color:'#fff',padding:'0 15px'}} value={search} onClick={handleOpenDialogSearch}/>
+            <DialogSearch openDialogSearch={openDialogSearch} setOpenDialogSearch={setOpenDialogSearch} selectCity={selectCity} setSelectCity={setSelectCity} setSearch={setSearch}/>
+        </Grid>
     )
 }
 
