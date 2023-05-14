b3f0e16e6a83983f80b42ca5f95f7e86bb270258
ms.moradi <ms.moradi28@gmail.com>
2023-05-14 16:56:12 +0430

    add outlet and style

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,8 +1,21 @@
-
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 const Layout =()=>{
     return(
         <>
+            <Main style={{
+                width:'100%',
+                height:'700px',
+                padding:'30px 20px',
+                display:'flex',
+                backgroundColor:'Black',
+                backgroundSize: 'cover',
+                opacity:'.9'
+                }}
+            >
+                <Outlet/>
+            </Main>
         </>
     )
 }
