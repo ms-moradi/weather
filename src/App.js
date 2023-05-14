636831402073d63590b4d7f9807983398d015cdb
ms.moradi <ms.moradi28@gmail.com>
2023-05-14 18:29:39 +0430

    add themeProvider and cssBaseline

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { RouterProvider } from 'react-router-dom';
+import router from "./route/route";
+import { ThemeProvider} from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import {theme} from './theme/theme';
 
-import './App.css';
 
 function App() {
   return (
-    <></>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <RouterProvider router={router}/> 
+    </ThemeProvider>
   );
 }
 
