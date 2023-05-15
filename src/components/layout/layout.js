88333bc69ef903f6de60ecd3b071fea60b5376fd
ms.moradi <ms.moradi28@gmail.com>
2023-05-15 11:32:38 +0430

    add background image and style

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,25 +1,26 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import background from "../../assets/images/farm.jpg";
 
 const Layout =()=>{
     return(
-        <>
+        <React.Fragment>
             <main style={{
-                width:'100%',
-                height:'700px',
-                padding:'30px 20px',
-                display:'flex',
-                justifyContent:'center',
-                alignContent:'center',
-                backgroundColor:'blue',
-                backgroundSize: 'cover',
-                opacity:'.9',
-                paddingTop:'50px'
+                 width:'100%',
+                 height:'700px',
+                 padding:'30px 20px',
+                 display:'flex',
+                 justifyContent:'center',
+                 backgroundImage:`url(${background})`,
+                 backgroundRepeat:'no-repeat',
+                 backgroundPosition: 'center',
+                 backgroundSize: 'cover',
+                 opacity:'.9'
                 }}
             >
                 <Outlet/>
             </main>
-        </>
+        </React.Fragment>
     )
 }
 
